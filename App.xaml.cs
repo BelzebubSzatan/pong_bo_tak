30b8a9246699ec22f390880dde6a7bf8f6b4ab32
KacperMatlag <matlagkacper@gmail.com>
2024-01-20 19:26:31 +0100

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace pong_bo_tak {
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application {
+    }
+
+}
